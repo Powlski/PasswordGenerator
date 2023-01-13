0a80838b864f71e873ce907f125f464292a0e268
Stephan Daschek <stedas@web.de>
2023-01-13 08:08:23 +0100

    add Spring dependencies, configure Spring, add PasswordController as Restcontroller, add first function RandomPassword

---
+++ src/main/java/Powlski/PasswordController.java
@@ -0,0 +1,22 @@
+package Powlski;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Random;
+
+@RestController
+public class PasswordController {
+
+    @GetMapping("RandomPassword/{length}")
+    public String RandomPassword(@PathVariable int length) {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!\"\\$%&/()=?";
+        String password = "";
+        int value;
+
+        for (int i = 0; i < length; i++)
+        {
+            value = new Random().ints(1,characters.length()).findFirst().getAsInt();
+            password += characters.charAt(value) ;
+        }
+        return password;
+    }
+}
